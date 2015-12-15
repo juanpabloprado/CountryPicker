241c68495e467751a80841fdb05994180c8fca79
Juan Pablo Prado <juan@juanpabloprado.com>
2015-12-15 02:47:02 -0500

    Add sample app

---
+++ sample/src/main/java/com/juanpabloprado/countrypicker/sample/MainActivity.java
@@ -0,0 +1,24 @@
+package com.juanpabloprado.countrypicker.sample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+import com.juanpabloprado.countrypicker.CountryAdapter;
+import com.juanpabloprado.countrypicker.CountryPicker;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    getSupportFragmentManager().beginTransaction()
+        .replace(R.id.container,
+            CountryPicker.getInstance(new CountryAdapter.CountryPickerListener() {
+              @Override public void onSelectCountry(String name, String code) {
+                Toast.makeText(MainActivity.this, "Name: " + name, Toast.LENGTH_SHORT).show();
+              }
+            }))
+        .commit();
+  }
+}
